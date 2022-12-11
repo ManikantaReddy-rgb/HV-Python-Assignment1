4947855d0baba0df0ba5e49aed76bbea09eea056
Manikanta Reddy <manikantareddy9848@gmail.com>
2022-12-11 12:01:35 +0530

    task1 and task2

--- assignment_question1.py
+++ assignment_question1.py
@@ -1,5 +1,33 @@
-num = [int(input("Enter Five numbers:")) for _ in range(5)]
+num1 = int(input("Enter first number:"))
+num2 = int(input("Enter second number:"))
+num3 = int(input("Enter third number:"))
+num4 = int(input("Enter fourth number:"))
+num5 = int(input("Enter fifth number:"))
 x = open("sumoutput.txt", "a")
-print("the given inputs are:",num,file=x)
-print("Sum of given inputs is:",sum(num))
-print("Sum of given inputs is:",sum(num), file=x)
+print("The given numbers are:",num1,num2,num3,num4,num5)
+print("The given numbers are:",num1,num2,num3,num4,num5,file=x)
+if (num1>0 and num2>0 and num3>0 and num4>0 and num5>0):
+    sum = num1+num2+num3+num4+num5
+    print("The sum of given numbers is:",sum)
+    print("The sum of given numbers is:",sum,file=x)
+else:
+    print("Enter positive numbers only")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# num = [int(input("Enter Five numbers:")) for _ in range(5)]
+# x = open("sumoutput.txt", "a")
+# print("the given inputs are:",num,file=x)
+# print("Sum of given inputs is:",sum(num))
+# print("Sum of given inputs is:",sum(num), file=x)
